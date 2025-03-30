359c04ba90e742a1512fc1430a58273329c25809
Alina <241516@astanait.edu.kz>
2025-03-30 19:45:25 +0500

    Added execution time measurement

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,9 @@ public class Main {
             System.out.println("Factorial of a negative number is undefined.");
             return;
         }
+
+        double startTime = System.nanoTime();
+
+
     }
 }
