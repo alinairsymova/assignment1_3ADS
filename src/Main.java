a4d4cc02ad3671d95062a1179b4e27b24c82e92e
Alina <241516@astanait.edu.kz>
2025-03-30 19:42:36 +0500

    Added user input handling

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number for factorial: ");
+        int number = scanner.nextInt();
     }
 }
