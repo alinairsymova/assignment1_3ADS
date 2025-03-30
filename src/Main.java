5e17f9f9a6a0b117d6530e51d18a28c2b0798ad1
Alina <241516@astanait.edu.kz>
2025-03-30 19:43:56 +0500

    Added validation for negative numbers

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,10 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter number for factorial: ");
         int number = scanner.nextInt();
+
+        if (number < 0) {
+            System.out.println("Factorial of a negative number is undefined.");
+            return;
+        }
     }
 }
