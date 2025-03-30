8a60d55b7fa97177d7fd1773f7ce8af76c242ceb
Alina <241516@astanait.edu.kz>
2025-03-30 19:52:24 +0500

    added comments

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,37 @@
+/**
+        This program calculates the factorial of a given number
+        Uses an iterative loop to calculate factorial
+        Calculate all factorials < 20
+        Time complexity: O(n) - linear complexity
+        A for loop iterates from 1 to n, multiplying each number to compute the factorial
+        It prints the factorial result and the execution time
+
+ **/
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter number for factorial: ");
-        int number = scanner.nextInt();
+        int number = scanner.nextInt(); // Ask user to enter a number of factorial
 
         if (number < 0) {
             System.out.println("Factorial of a negative number is undefined.");
-            return;
+            return;  // Chek if there is negative number
         }
 
-        double startTime = System.nanoTime();
+        double startTime = System.nanoTime(); // Records the start time using
 
         long factorial = 1;
         for (int i = 1; i <= number; i++) {
             factorial *= i;
+            // Calculate factorial by iterations
         }
+
         double endTime = System.nanoTime();
-        double duration = (endTime - startTime) / 1000000 ;
+        double duration = (endTime - startTime) / 1000000 ; // Records the end time and calculates the execution time in milliseconds
 
         System.out.println("Factorial of " + number + " is " + factorial);
-        System.out.println("Execution time: " + duration );
+        System.out.println("Execution time: " + duration ); // Print result of time duration and factorial of num
     }
 }
